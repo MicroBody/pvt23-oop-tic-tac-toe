e1d257e6408d4f99fbf90e84f4b4d6ae8bf0663a
Ironboy <ironboy@users.noreply.github.com>
2024-09-02 15:12:51 +0200

    some initial test examples with vitest + happy dom

--- tests/helpers/sleep.js
+++ tests/helpers/sleep.js
@@ -1,5 +1,4 @@
 // call with await to pause/sleep for a number of milliseconds
 export default function sleep(ms) {
-  if (globalThis.mockMinimalSleep) { return; }
   return new Promise(resolve => setTimeout(resolve, ms));
 }
