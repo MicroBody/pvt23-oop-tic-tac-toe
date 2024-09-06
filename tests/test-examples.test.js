f0a98ac54ca08bbdfc6d4a64541fb4ed10c2852e
Ironboy <ironboy@users.noreply.github.com>
2024-09-06 08:00:51 +0200

    code cleanup

--- tests/test-examples.test.js
+++ tests/test-examples.test.js
@@ -1,28 +1,20 @@
 import { expect, test } from 'vitest';
-import getDocument from './helpers/getDocument.js';
-import waitUntil from './helpers/waitUntil.js';
-import click from './helpers/triggerOnclick.js';
-import sleep from './helpers/sleep.js';
+
+// some helpers for mocking
+import getDocument from './helpers/mock-help/getDocument.js';
+import click from './helpers/mock-help/triggerOnclick.js';
+
+// other helpers for mocking not used right now
+// import sleep from './helpers/mock-help/getDocument.js';
+// import waitUntil from './helpers/mock-help/waitUntil.js';
+
+// A common task - something we do in the prgoram flow
+// in several tests
+import registerPlayers from './helpers/commonTasks/registerPlayers.js';
+
+// The App class from the program
 import App from '../classes/App.js';
 
-
-// make the program sleep less (see classes/helpers/sleep.js)
-globalThis.mockMinimalSleep = true;
-
-// in several test we want to register the players by entering their names
-// do this (including creating a new App) and return the body after its done
-async function registerPlayers() {
-  let { body } = getDocument();
-  globalThis.mockAnswers = ['Anna', 'Beata'];
-  let app = new App();
-  // wait until the dom does not have p tag in the main tag with 'Enter names'
-  await waitUntil(() =>
-    !body.querySelector('main p').innerText.includes('Enter names'));
-  // has the app registered the players
-  expect(app.playerX.name).toBe('Anna');
-  expect(app.playerO.name).toBe('Beata');
-  return body;
-}
 
 test('Does the logo/headline have the text "Tic-Tac-Toe" ?', () => {
   let { body } = getDocument();
@@ -50,4 +42,4 @@ test('Make the first two moves and check they are appear on the board', async ()
   expect(body.querySelector('.cell:nth-child(5)').classList.contains('X')).toBeTruthy();
   click(body.querySelector('.cell:nth-child(4)'));
   expect(body.querySelector('.cell:nth-child(4)').classList.contains('O')).toBeTruthy();
-})
+});
